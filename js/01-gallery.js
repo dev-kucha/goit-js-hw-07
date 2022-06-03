a420d2cc5ce0db00cf5e22528e0ace4f8376a5cf
Yurii <ukucher@gmail.com>
2022-06-03 21:10:52 +0300

    1.1. done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// 1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
+const galleryContainer = document.querySelector('.gallery');
+// console.log(galleryContainer);
+
+const galleryMarkup = galleryItems.map(galleryItem => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>`;
+});
+// console.log(galleryMarkup.join(''));
+
+galleryContainer.innerHTML = galleryMarkup.join('');
